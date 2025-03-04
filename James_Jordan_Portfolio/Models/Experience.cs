7d8c0451f5eeadb4e427b35ee3c1f9f7c028fac0
Jay Jordan <jamescyriljordan@gmail.com>
2025-03-04 12:25:49 -0500

    - project reset

--- James_Jordan_Portfolio/Models/Experience.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace James_Jordan_Portfolio.Models
-{
-	public class Experience
-	{
-		public int Id { get; set; }
-	}
-}
--- Portfolio.Web/Program.cs
+++ Portfolio.Web/Program.cs
@@ -1,25 +1,23 @@
-using James_Jordan_Portfolio.Data;
-using Microsoft.EntityFrameworkCore;
+using Portfolio.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<PortfolioDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Dependency Injection
+builder.Services.AddPortfolioServices(builder.Configuration); // This sets up EF Core via Portfolio.Services
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
